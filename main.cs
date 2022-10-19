08665de8f935b42aba8cbe41d9f6dbd8ff89e55a
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-19 18:06:44 +0000

    spaces

--- main.cs
+++ main.cs
@@ -9,7 +9,7 @@ class Program
         double pay;
         double tax;
         double TAX_RATE;
-  
+
         Console.WriteLine("Salary Program with Csharp");
         Console.WriteLine("");
 
@@ -17,16 +17,16 @@ class Program
         hours = Convert.ToInt32(Console.ReadLine());
         Console.Write("hourly rate: $ ");
         rate = Convert.ToInt32(Console.ReadLine());
-  
+
         TAX_RATE = 0.18;
         pay = (hours * rate) * (1.00 - TAX_RATE);
         tax = (hours * rate) * TAX_RATE;
-    
+
         Console.WriteLine("");
         Console.WriteLine("The pay is: " + pay.ToString("0.00") + ". ");
-  
+
         pay.ToString("0.00"); // returns "0.50"  when decimalVar == 0.5m
- 
+
         Console.WriteLine("The tax is: " + tax.ToString("0.00") + ". ");
 
         tax.ToString("0.00"); // returns "0.50"  when decimalVar == 0.5m
